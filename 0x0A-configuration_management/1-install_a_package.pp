60f5d043dde69814067fa8472f5eb2051333f1e2
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-11-24 18:24:44 +0000

    Added certainity check for pre-requisite packages for task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,17 @@
 # This script installs flask from pip3
 
+package { 'python3':
+	ensure  => installed,
+}
+
+package { 'python3-pip':
+	ensure => installed,
+	require => Package['python3'],
+}
+
 package { 'Flask':
     ensure   => '2.1.0',
-    provider => 'pip3',
+    provider => 'pip',
+	require  => Package['python3-pip'],
 }
 
