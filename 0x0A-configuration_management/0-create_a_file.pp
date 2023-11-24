eebc1631e3252723b80157c4da265a47cd409c0e
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-11-24 19:42:43 +0000

    Fixed syntax error in task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 #This manifest creates a file named school in /tmp
 
 file { '/tmp/school':
-    mode    => '0755'
+    mode    => '0755',
     content => 'I love Puppet',
     owner   => 'www-data',
     group   => 'www-data',
