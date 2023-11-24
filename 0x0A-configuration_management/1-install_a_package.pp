1fd3d8ed3a9c3f06e082c031c17f7fc95605b575
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-11-24 18:30:02 +0000

    Fixed linting errors

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,17 @@
 # This script installs flask from pip3
 
 package { 'python3':
-	ensure  => installed,
+  ensure  => installed,
 }
 
 package { 'pip3':
-	ensure => installed,
-	require => Package['python3'],
+  ensure  => installed,
+  require => Package['python3'],
 }
 
 package { 'Flask':
     ensure   => '2.1.0',
     provider => 'pip3',
-	require  => Package['pip3'],
+    require  => Package['pip3'],
 }
 
