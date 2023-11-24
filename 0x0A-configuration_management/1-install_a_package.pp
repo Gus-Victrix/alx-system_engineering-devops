9def055178f3a3310f158241107ad09d73946250
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-11-25 01:15:11 +0300

    Update 1-install_a_package.pp
    done on mobile

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,6 +3,6 @@
 exec { 'install_flask':
   command => 'pip3 install Flask==2.1.0',
   path    => '/usr/local/bin:/usr/bin:/bin',
-  creates => '/usr/local/bin/flask',  # Adjust the path based on your system
-  require => Package['python3-pip'],  # Ensure pip3 is installed first
+  creates => '/usr/local/bin/flask',
+  require => Package['python3-pip'],
 }
