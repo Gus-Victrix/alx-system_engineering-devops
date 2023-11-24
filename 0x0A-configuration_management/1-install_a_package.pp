481658d21f71270123897bf79648ef3d9296b252
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-11-24 20:33:00 +0000

    Gpt failed too. Revert "Using gpt generated code"
    This reverts commit cb29fd65b0e6ffbde69c79ec71acdb524ecdd019.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,29 +1,8 @@
-# In the 'python' class
-$python_pkg = $operatingsystem ? {
-  ubuntu => 'python3',
-  default => 'python3',  # Defaulting to python3 for non-specified systems
-}
+# install_flask.pp
 
-package { $python_pkg:
-  ensure => installed,
-}
-
-# In the 'python3-pip' class
-$python_pip_pkg = $operatingsystem ? {
-  ubuntu => 'python3-pip',
-  default => 'python3-pip',  # Defaulting to python3-pip for non-specified systems
-}
-
-package { $python_pip_pkg:
-  ensure  => installed,
-  require => Package[$python_pkg],
-}
-
-# In the 'flask' class
-$flask_version = '2.1.0'
-
-exec { "install_flask_${flask_version}":
-  command => "/usr/bin/pip3 install Flask==${flask_version}",
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin:/bin',
   creates => '/usr/local/bin/flask',  # Adjust the path based on your system
-  require => Package[$python_pip_pkg],
+  require => Package['python3-pip'],  # Ensure pip3 is installed first
 }
