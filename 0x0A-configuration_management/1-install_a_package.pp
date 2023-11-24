dab7dcaa77d994b40db102bd6029d1437182753e
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-11-24 18:27:26 +0000

    Some updates to task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,14 +4,14 @@ package { 'python3':
 	ensure  => installed,
 }
 
-package { 'python3-pip':
+package { 'pip3':
 	ensure => installed,
 	require => Package['python3'],
 }
 
 package { 'Flask':
     ensure   => '2.1.0',
-    provider => 'pip',
-	require  => Package['python3-pip'],
+    provider => 'pip3',
+	require  => Package['pip3'],
 }
 
