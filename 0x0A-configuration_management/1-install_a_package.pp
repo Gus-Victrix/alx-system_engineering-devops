9eb3cb27b1fe429ec55875bae9dd970e81d7cd5b
Francis Njoroge Nderitu <iamfrsncis@gmail.com>
2023-11-25 21:29:37 +0000

    Fixed errors in task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,17 @@
 # install_flask.pp
 
-exec { 'install_flask':
-  command => 'pip3 install Flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  creates => '/usr/local/bin/flask',
-  require => Package['python3-pip'],
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Werkzeug':
+  ensure   => '2.0.2',  # Adjust the version as needed
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['Werkzeug'],
 }
