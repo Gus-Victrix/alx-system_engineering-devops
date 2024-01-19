4a0ea78b0da1b05361b6bed317d4a64a7ec87c9b
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-20 00:45:52 +0300

    :fire: refactor: Removed extra code.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,36 +1,7 @@
 #!/usr/bin/puppet
 # install_flask.pp
 
-package { 'python3-pip':
-  ensure   => installed,
-  provider => apt,
-  update   => true,
-}
-
-package { 'Werkzeug':
-  ensure   => '2.0.2',  # Adjust the version as needed
-  provider => 'pip3',
-  require  => Package['python3-pip'],
-  update   => true,
-}
-
-package { 'Jinja2':
-  ensure   => '3.0.1',
-  provider => 'pip3',
-  require  => Package['Werkzeug'],
-  update   => true,
-}
-
-package { 'itsdangerous':
-  ensure   => '2.0.1',
-  provider => 'pip3',
-  require  => Package['Werkzeug'],
-  update   => true,
-}
-
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['Werkzeug'],
-  update   => true,
 }
