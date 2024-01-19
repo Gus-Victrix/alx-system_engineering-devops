1a6c0ccf88722fdb48662dadd1baa623a482f63a
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-20 01:35:33 +0300

    :sparkles: feature: Now 1-install_a_package.pp installs all dependencies of Flask before installing it.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,23 @@
 #!/usr/bin/puppet
 # install_flask.pp
 
+package { 'python3':
+  ensure => 'latest',
+  provider => 'apt',
+}
+
+package { 'python3-pip':
+  ensure => 'latest',
+  require => Package['python3'],
+  provider => 'apt',
+}
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
