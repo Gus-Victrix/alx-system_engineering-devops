fe318a88df8fcb0a1367165901ae282005e458be
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-20 01:43:55 +0300

    :rotating_light: removed trailing whitespace

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,19 @@
 #!/usr/bin/puppet
-# install_flask.pp
+# install flask version
 
 package { 'python3':
-  ensure => 'latest',
+  ensure   => 'latest',
   provider => 'apt',
 }
 
 package { 'python3-pip':
-  ensure => 'latest',
-  require => Package['python3'],
+  ensure   => 'latest',
+  require  => Package['python3'],
   provider => 'apt',
 }
+
 package { 'werkzeug':
-  ensure   => '2.0.1',
+  ensure   => '2.1.1',
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
