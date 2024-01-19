2d5ff8022f316457a1fa94a48789aa2a3935982a
Gus-Victrix <iamfrsncis@gmail.com>
2024-01-20 00:17:16 +0300

    :sparkles: feature: Now puppet does not re-create the file if it exists.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #!/usr/bin/puppet
-
 #This manifest creates a file named school in /tmp
 
 file { '/tmp/school':
+    ensure  => 'present',
     mode    => '0744',
-    content => "I love Puppet\n",
+    content => 'I love Puppet',
     owner   => 'www-data',
     group   => 'www-data',
 }
