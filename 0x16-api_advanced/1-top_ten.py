05d9b040c9ca9e3b73d8c118d706df4c79e90814
Gus-Victrix <iamfrsncis@gmail.com>
2024-03-06 05:51:03 +0300

    file: Lists top posts of a sub-reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Top Ten posts of a subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'Extra-Ordinary user'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json().get('data')
+    children = data.get('children')
+    for child in children:
+        print(child.get('data').get('title'))
