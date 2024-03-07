89f6d1cd4fd6d38b693873f17443b18f52ed88ca
Gus-Victrix <iamfrsncis@gmail.com>
2024-03-07 05:15:33 +0300

    file: Using puppet to fix php

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+#!/usr/bin/puppet
+# Fix the server because someone wrote bad php
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
